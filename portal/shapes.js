038d9b45c19ac84618cad20b2f8036360ea6c9dc
Nikolas Martens <nikolas.m@rtens.org>
2025-02-21 19:49:13 +0100

    No optimization

--- portal/shapes.js
+++ portal/shapes.js
@@ -1,20 +1,13 @@
 import { len, add, neg } from './math.js'
 
-const bound = (l, shape) => p => {
-  const b = Math.max(...p.map(Math.abs))
-  if (b > l + .1) return b - l
-  return shape(p)
-}
+export const sphere = p => len(p) - 1
 
-export const sphere = bound(1, p =>
-  len(p) - 1)
-
-export const box = b => bound(Math.max(...b), p => {
+export const box = b => p => {
   const q = add(p.map(Math.abs), neg(b))
   return len(q.map(n => Math.max(n, 0))) + Math.min(Math.max(...q, 0), 0);
-})
+}
 
-export const torus = ([tx, ty]) => bound(tx + ty, ([x, y, z]) => {
+export const torus = ([tx, ty]) => ([x, y, z]) => {
   const q = [len([x, z]) - tx, y]
   return len(q) - ty
-})
+}
